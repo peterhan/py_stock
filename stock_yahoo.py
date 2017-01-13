56ae41c8ccdba9d2934a6146238a663965ab8b5d
hanshu <peter.hanshu@gmail.com>
2017-01-13 09:26:29 +0800

    stock yahoo

---
+++ stock_yahoo.py
@@ -0,0 +1,8 @@
+#intro:http://www.jarloo.com/yahoo_finance/
+http://finance.yahoo.com/d/quotes.csv?s=AAPL+GOOG+MSFT&f=nab
+#http://stackoverflow.com/questions/25097779/getting-stocks-historical-data
+http://query.yahooapis.com/v1/public/yql?q=show%20tables
+#http://www.jarloo.com/get-yahoo-finance-api-data-via-yql/
+https://developer.yahoo.com/weather/
+#
+http://ichart.finance.yahoo.com/table.csv?s=AAPL&a=00&b=01&c=2015&d=0&e=12&f=2017&g=d
