415c388a593df5d4f8e47f00673868bafb1b26ed
hanshu <peter.hanshu@gmail.com>
2019-08-23 10:26:35 +0800

    提交

--- tushare_apply/talib_stk_monitor.py
+++ tushare_apply/talib_stk_monitor.py
@@ -107,10 +107,11 @@ def tech_analyse(info,tk, df):
     vol = df['volume'].values    
     
     bl_upper, bl_middle, bl_lower = talib.BBANDS(close,timeperiod=15, nbdevup=1, nbdevdn=1, matype=0)
-    u = talib.LINEARREG_ANGLE(bl_upper, timeperiod=2)
-    m = talib.LINEARREG_ANGLE(bl_middle,timeperiod=2)
-    l = talib.LINEARREG_ANGLE(bl_lower, timeperiod=2)
-    print 'Boll',[u[-1],m[-1],l[-1]]
+    idx=100.0/bl_middle[-1]
+    u = talib.LINEARREG_ANGLE(bl_upper*idx, timeperiod=2)
+    m = talib.LINEARREG_ANGLE(bl_middle*idx,timeperiod=2)
+    l = talib.LINEARREG_ANGLE(bl_lower*idx, timeperiod=2)
+    print 'BollAngle',[u[-1],m[-1],l[-1]]
     macd, macdsignal, macdhist =  talib.MACD(close,fastperiod=6, slowperiod=12, signalperiod=9)
     roc = talib.ROCR(close)
     slk,sld = talib.STOCH(high,low,close)
@@ -200,7 +201,8 @@ def print_analyse_res(res):
         if stock['cdl'] != None:
             cdl = stock['cdl']
             cdl_ent_str=','.join([u'[{}:{}]:{}{}'.format(info['score'],info['figure'],name,info['cn_name']) for name,info in cdl['data'].items()])
-            intro[info['en_name']+info['cn_name']] = info['intro2']
+            for name,info in cdl['data'].items():
+                intro[info['en_name']+info['cn_name']] = info['intro2']
             print "[CDL:{0}]; {1}".format(cdl['cdl_total'], cdl_ent_str.encode('gbk'))
     for name,intro in intro.items():
         print u"[{}]:{}".format(name,intro).encode('gbk')
@@ -254,8 +256,6 @@ def main_loop(mode):
         res = [job.value for job in jobs]
     json.dump(res,open('result.json','w'),indent=2)
     print_analyse_res(res)
-        
-       
         
     # raw_input("pause")
  
