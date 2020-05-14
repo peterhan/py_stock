e1a8424c434c276c9c28ba18706f600b1b0b4933
hanshu <peter.hanshu@gmail.com>
2020-05-15 06:20:41 +0800

    emd

--- tushare_apply/stock_emd.py
+++ tushare_apply/stock_emd.py
@@ -3,16 +3,18 @@ from pyhht.visualization import plot_imfs
 import tushare as ts
 import matplotlib.pyplot as plt
 
-def emd_plot(tick):
-    df = ts.get_hist_data(tick)    
-    df = df.sort_index()
-    emd = EMD(df['close'])
+
+def emd_plot(data):
+    emd = EMD(data)
     imfs = emd.decompose()    
-    plot_imfs(df['close'],imfs)
+    plot_imfs(data,imfs)
 
 
 if __name__=='__main__':
-    emd_plot('600438')
+    df = ts.get_hist_data('600438')    
+    df = df.sort_index()
+    emd_plot(df['close'])
+    emd_plot(df['close'][:-50])
     # emd_plot('600004')
     # emd_plot('601138')
     # emd_plot('518800')
