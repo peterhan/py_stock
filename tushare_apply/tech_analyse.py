72756262324c89f02481bfe7273ca9c6c29aacfb
hanshu <peter.hanshu@gmail.com>
2020-11-10 09:34:16 +0800

    add

--- tushare_apply/tech_analyse.py
+++ tushare_apply/tech_analyse.py
@@ -411,11 +411,16 @@ def test():
     def pprint(info, indent=None):
         print json.dumps(info, ensure_ascii=False, indent=2).encode('gbk')
     
-    # df = ts.get_hist_data('601865')
-    df = ts.get_hist_data('600438')
-    # df = df.sort_index()
+    remote_call = False
+    remote_call = True
+    if remote_call:
+        df = ts.get_hist_data('601865')
+        # df = ts.get_hist_data('600438')
+        df = df.sort_index()
+        df.to_csv('veri/origin.csv')
+    
     # pdb.set_trace()
-    df.to_csv('veri/origin.csv')
+    
     df=pd.read_csv('veri/origin.csv',index_col='date')
     df['date'] = df.index
     # df = df.sort_values('date')
@@ -432,13 +437,13 @@ def test():
     # pprint(cinfo)
     # print df.tail(1)
     ###
-    verify_indicator(df)
+    i_stages = [['cci_stage','ema_stage','sma_stage','ma_es_dif_stage','macd_stage','boll_stage','rsi_stage','kdj_stage'] ]    
+    i_stage_list = [ ['cci_stage'] , ['ema_stage']  ,['sma_stage'] ,['macd_stage'] ,['rsi_stage'] ,['ma_es_dif_stage'],['boll_stage'] ,['kdj_stage'] ]
+    for i_stages in i_stage_list:
+        verify_indicator(df,i_stages)
     
-def verify_indicator(df):
-    i_stages = ['cci_stage','ema_stage','sma_stage','ma_es_dif_stage','macd_stage','boll_stage','rsi_stage','kdj_stage'] 
-    # i_stages = ['ema_stage','sma_stage','ma_es_dif_stage','macd_stage'] 
-    i_stages = ['macd_stage'] 
-    # i_stages = ['ema_stage'] 
+def verify_indicator(df,i_stages):
+
     # i_stages = cdl_pat_names
     adf = df[['turnover','rsi','dif_ag','rsi_ag','dif','k_ag','j_ag','d_ag','dea']+i_stages].copy()
     bencols = []
@@ -464,10 +469,10 @@ def verify_indicator(df):
     # df.to_csv('veri/comb.csv')
     for stage in i_stages:
         df = stat_gp(adf.groupby(stage )[bencols])
-        print df
+        # print df
         # df.to_csv('veri/'+stage+".csv")       
     # pdb.set_trace()
-    target_col='p_change_3d'
+    target_col='p_change_15d'
     train_cat(adf,i_stages,target_col)
     predict_cat(adf,i_stages,target_col)
 
@@ -484,19 +489,30 @@ def predict_cat(adf,i_stages,target_col):
     # preds_class = model.predict(test_pool, prediction_type='Class')
     # preds_proba = model.predict(test_pool, prediction_type='Probability')
     preds_raw_vals = model.predict(test_pool, prediction_type='RawFormulaVal')
-    pdb.set_trace()
-    from matplotlib import pyplot as plt
+    # pdb.set_trace()
+    
+    ss = [[k] for k in  test_pool[i_stages[0]].value_counts().index]
+    ps = model.predict(ss)
+    sts = [row[0] for row in ss]
+    pdf  = pd.DataFrame({'feature':sts,'weight':ps})
+    print i_stages
+    print pdf
+    
     # print preds_raw_vals,test_labels
 
-    rmsev = rmse(test_labels.values, preds_raw_vals)
-    print 'rmse:',rmsev
+    rmsev = rmse( np.sign(test_labels.values), np.sign(preds_raw_vals) )
+    print 'rmse: %0.2f'%rmsev
     pos_neg = pd.Series(np.sign(test_labels*preds_raw_vals)*10,index=test_labels.index)
-    print 'pos_neg',pos_neg.value_counts()
-    plt.title(i_stages)
+    pnvc = pos_neg.value_counts()
+    
+    print 'correct_rate: %0.2f%%'%(pnvc[10.0]*1.0/sum(pnvc.values)*100)
+    
+    from matplotlib import pyplot as plt
+    plt.title('%s %s'%(i_stages,target_col))
     plt.plot(preds_raw_vals[:],'--')
     plt.plot(test_labels.values[:])
     plt.plot(pos_neg.values[:],':')
-    plt.show()
+    # plt.show()
     # return preds_class,preds_proba,preds_raw_vals
 
 def train_cat(adf,i_stages,target_col):
@@ -504,7 +520,7 @@ def train_cat(adf,i_stages,target_col):
     dataset = adf[i_stages][:-50]
     train_labels = adf[target_col].fillna(0)[:-50]
     model = CatBoostRegressor(learning_rate=1, depth=6, loss_function='RMSE',cat_features=i_stages)
-    fit_model = model.fit(dataset, train_labels)
+    fit_model = model.fit(dataset, train_labels, verbose=0)
 
     # print(fit_model.get_params())
     fit_model.save_model('first.model')
