cca08f299b8453a569dd121d4233c280e20a665c
hanshu <peter.hanshu@gmail.com>
2021-06-11 10:27:22 +0800

    add

--- stk_console/tech_cross_type.py
+++ stk_console/tech_cross_type.py
@@ -5,13 +5,18 @@ import talib
 from matplotlib import pyplot as plt
 
 
-def get_angle(ss,p=4):
+def get_angle(ss,p=4,ang_type='degree'):
     fss = np.nan_to_num(ss,0)
-    scale = (max(fss)-min(fss))/2*1.1
+    scale = (max(fss)-min(fss))/2*1.618
     nfss = fss/scale    
     # pdb.set_trace()
-    angle =  talib.LINEARREG_ANGLE(nfss, timeperiod=p)    
-    return angle
+    angle =  talib.LINEARREG_ANGLE(nfss, timeperiod=p)
+    if ang_type=='degree':
+        return np.arctan(angle)/np.pi*180
+    elif ang_type =='pi':
+        return np.arctan(angle)
+    else:
+        return angle
     
 def get_crossx_type(fast_line,slow_line):
     fast_ag = get_angle(fast_line, 2)
@@ -44,19 +49,20 @@ def get_crossx_type(fast_line,slow_line):
     return df
     
 if __name__ == '__main__':
-    fast_s=np.sin(np.arange(80)*0.1+0.5)*5
-    slow_s=np.sin(np.arange(80)*0.1)*5
+    fast_s=np.sin(np.arange(80)*0.1+0.5)*1
+    slow_s=np.sin(np.arange(80)*0.1)*1
     res = get_crossx_type(fast_s,slow_s)
     print res.columns.to_list()
     for row in res.iterrows():
         print row[1].to_list()
     print res.groupby('cross_stage')['cross_stage'].count()
-    ag = get_angle(fast_s)
-    print 'angle:',ag
-    print 'angle:',np.arctan(ag)/np.pi*180*0.01
+    fag = get_angle(fast_s)
+    sag = get_angle(slow_s)    
     # pdb.set_trace()
     plt.plot(fast_s)
-    # plt.plot(slow_s)
-    plt.plot(np.arctan(ag)/np.pi*180*0.01)
-    plt.plot(ag)
+    plt.plot(slow_s)
+    plt.plot(fag*0.01)
+    plt.plot(sag*0.01)
+    plt.plot((fag-sag)*0.01)
+    # plt.plot(np.arctan(ag)/np.pi*180*0.01)
     plt.show()
